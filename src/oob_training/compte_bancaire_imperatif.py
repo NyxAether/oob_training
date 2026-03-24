f8198ea9130306ab9e48a35051a0440bfdb542ec
NyxAether <contact.nyxhemera@gmail.com>
2026-03-24 16:17:54 +0100

    solution final

--- src/oob_training/compte_bancaire_imperatif.py
+++ src/oob_training/compte_bancaire_imperatif.py
@@ -6,30 +6,48 @@ Compte = tuple[TypeCompte, int, str, str | None]
 def creer_compte(
     type_compte: TypeCompte, proprietaire: str, proprietaire2: str | None = None
 ) -> Compte:
-    # Votre code ici
-    pass
+    assert (type_compte is TypeCompte.COMPTE_JOINT) != (proprietaire2 is None)
+    return type_compte, 0, proprietaire, proprietaire2
+
+
+def _verifier_proprietaire(compte: Compte, demandeur: str) -> None:
+    if not est_proprietaire(compte, demandeur):
+        raise ValueError(f"{demandeur} n'est pas autorisé à agir sur le compte.")
+
+
+def _set_montant(compte: Compte, montant: int) -> Compte:
+    type_compte, solde, p1, p2 = compte
+    if type_compte is TypeCompte.LIVRET_A and solde + montant > 29500:
+        raise ValueError("Seuil maximal de 29 500 € atteint")
+    if type_compte is TypeCompte.COMPTE_DEPOT and solde + montant < 0:
+        raise ValueError("Solde insuffisant")
+    return type_compte, solde + montant, p1, p2
 
 
 def depot(compte: Compte, demandeur: str, montant: int) -> Compte:
-    # Votre code ici
-    pass
+    _verifier_proprietaire(compte, demandeur)
+    return _set_montant(compte, montant)
 
 
 def retrait(compte: Compte, demandeur: str, montant: int) -> Compte:
-    # Votre code ici
-    pass
+    _verifier_proprietaire(compte, demandeur)
+    return _set_montant(compte, -montant)
 
 
 def transfert(
-    compte_source: Compte,
-    compte_cible: Compte,
-    demandeur: str,
-    montant: int,
+    compte_source: Compte, compte_cible: Compte, demandeur: str, montant: int
 ) -> tuple[Compte, Compte]:
-    # Votre code ici
-    pass
+    _verifier_proprietaire(compte_source, demandeur)
+    compte_source = _set_montant(compte_source, -montant)
+    compte_cible = _set_montant(compte_cible, montant)
+    return compte_source, compte_cible
 
 
 def solde(compte: Compte, demandeur: str) -> int:
-    # Votre code ici
-    pass
+    _verifier_proprietaire(compte, demandeur)
+    return compte[1]
+
+
+def est_proprietaire(compte: Compte, personne: str) -> bool:
+    _, _, p1, p2 = compte
+    return personne == p1 or p2 is not None and personne == p2
--- src/oob_training/compte_bancaire_oob.py
+++ src/oob_training/compte_bancaire_oob.py
@@ -1,40 +1,62 @@
 class Compte:
-    def __init__(self, proprietaire: str):
-        # Votre code ici
-        NotImplemented
+    def __init__(self, proprietaire: str) -> None:
+        self._proprietaire = proprietaire
+        self._solde = 0
 
-    def solde(self, demandeur: str) -> int:
-        # Votre code ici
-        NotImplemented
+    def _verifier_proprietaire(self, demandeur: str) -> None:
+        if not self.est_proprietaire(demandeur):
+            raise ValueError(f"{demandeur} n'est pas autorisé à agir sur le compte.")
+
+    def _ajoute_solde(self, montant: int) -> None:
+        self._solde += montant
 
     def depot(self, demandeur: str, montant: int) -> None:
-        # Votre code ici
-        NotImplemented
+        self._verifier_proprietaire(demandeur)
+        self._ajoute_solde(montant)
 
     def retrait(self, demandeur: str, montant: int) -> None:
-        # Votre code ici
-        NotImplemented
+        self._verifier_proprietaire(demandeur)
+        self._ajoute_solde(-montant)
 
-    def transfert(self, other: "Compte", demandeur: str, montant: int) -> None:
-        # Votre code ici
-        NotImplemented
+    def solde(self, demandeur: str) -> int:
+        self._verifier_proprietaire(demandeur)
+        return self._solde
+
+    def transfert(self, autre: "Compte", demandeur: str, montant: int) -> None:
+        self._verifier_proprietaire(demandeur)
+        self._ajoute_solde(-montant)
+        try:
+            autre._ajoute_solde(montant)
+        except ValueError as e:
+            self._ajoute_solde(montant)
+            raise e
+
+    def est_proprietaire(self, proprietaire: str) -> bool:
+        return self._proprietaire == proprietaire
 
 
 class CompteCourant(Compte):
-    # Votre code ici
     pass
 
 
 class CompteDepot(Compte):
-    # Votre code ici
-    pass
+    def _ajoute_solde(self, montant: int) -> None:
+        if self._solde + montant < 0:
+            raise ValueError("Pas de découvert possible.")
+        super()._ajoute_solde(montant)
 
 
 class LivretA(Compte):
-    # Votre code ici
-    pass
+    def _ajoute_solde(self, montant: int) -> None:
+        if self._solde + montant > 29500:
+            raise ValueError("Plafond dépassé.")
+        super()._ajoute_solde(montant)
 
 
 class CompteJoint(Compte):
-    # Votre code ici
-    pass
+    def __init__(self, proprietaire: str, proprietaire2: str) -> None:
+        super().__init__(proprietaire)
+        self._proprietaire2 = proprietaire2
+
+    def est_proprietaire(self, proprietaire: str) -> bool:
+        return proprietaire in {self._proprietaire, self._proprietaire2}
